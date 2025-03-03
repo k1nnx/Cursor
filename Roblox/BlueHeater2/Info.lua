1239a69ad1257d2723862b3f248ea657eed3535f
Lazytive <Lazytive@gmail.com>
2025-03-03 23:30:20 +0000

    d

--- Roblox/BlueHeater2/Info.lua
+++ Roblox/BlueHeater2/Info.lua
@@ -1,4 +1,13 @@
-local function GetEntityInfo()
+-- Create a global API for BlueHeater2 entity info
+if not _G.BlueHeaterAPI then
+    _G.BlueHeaterAPI = {}
+end
+
+local EntityInfoAPI = {}
+_G.BlueHeaterAPI.EntityInfo = EntityInfoAPI
+
+-- Function to get all entity information
+function EntityInfoAPI.GetAllEntities()
     local entityInfos = {}
     local children = workspace:WaitForChild("SpawnedEntities"):GetChildren()
 
@@ -64,4 +73,30 @@ local function GetEntityInfo()
     return entityInfos
 end
 
-return GetEntityInfo()
+-- Function to find entity by name
+function EntityInfoAPI.FindEntityByName(name)
+    local entities = EntityInfoAPI.GetAllEntities()
+    for _, entity in ipairs(entities) do
+        if entity.Name == name then
+            return entity
+        end
+    end
+    return nil
+end
+
+-- Print summary
+function EntityInfoAPI.PrintSummary()
+    local entities = EntityInfoAPI.GetAllEntities()
+    print("--- BlueHeater2 Entity Info Summary ---")
+    print("Total Entities Found: " .. #entities)
+    for i, entity in ipairs(entities) do
+        print(i .. ". " .. entity.Name .. " - HP: " .. entity.Health .. "/" .. entity.MaxHealth)
+    end
+    print("-------------------------------------")
+end
+
+-- Run summary on load
+EntityInfoAPI.PrintSummary()
+
+-- Return the API for module-style usage as well
+return EntityInfoAPI
